c80b8b525d52cd126777a0a21128524dc7ff19d5
Sumi <equepem@gmail.com>
2023-08-29 14:20:53 +0900

    font and image

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const NavBar = (): JSX.Element => {
   return (
     <nav>
+      {/*  <Image src={} alt = {}/> */}
       <Link href="/">Home</Link>
       <Link href="/education">Education</Link>
     </nav>

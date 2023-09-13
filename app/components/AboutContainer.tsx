0a55dc7a9de2953ae16529ea13123bf2839d47fb
Sumi <equepem@gmail.com>
2023-09-13 10:59:17 +0530

    feature/change-about-page-scss

--- app/components/AboutContainer.tsx
+++ app/components/AboutContainer.tsx
@@ -6,15 +6,18 @@ import ContentBar from "./ContentBar";
 export default function AboutContainer(): JSX.Element {
   return (
     <div className={styles.about}>
-      <div className="about__container">
+      <div className={styles.about__container}>
         <div className={styles.about__box}>
           <h1>My mission</h1>
         </div>
         <div className={styles.about__box}>
-          <h4>explanation</h4>
+          <h4>
+            Create a generalized brain interface to restore autonomy to those
+            with unmet medical needs today and unlock human potential tomorrow.
+          </h4>
         </div>
       </div>
-      <div className="about__container">
+      <div className={styles.about__container}>
         <div className={styles.about__box}>
           <ContentBar />
           <ContentBar />

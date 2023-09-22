725b57b1b8f7ea085c0881f66b15eb138a3c7f77
Sumi <equepem@gmail.com>
2023-09-22 16:50:29 +0200

    feature/about-container-add-scss-styling: '

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,5 +1,10 @@
 import styles from "../styles/components/Content.module.scss";
 
 export default function Content(): JSX.Element {
-  return <div className={styles.content}>Content </div>;
+  return (
+    <div className={styles.content}>
+      <div className={styles.content__text}>implant</div>
+      <div className={styles.content__img}>img</div>
+    </div>
+  );
 }

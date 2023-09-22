853f6557d7be65de33f76415f2be1805086874f4
Sumi <equepem@gmail.com>
2023-09-22 17:01:42 +0200

    feature/about-container-modify-styling

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -3,7 +3,14 @@ import styles from "../styles/components/Content.module.scss";
 export default function Content(): JSX.Element {
   return (
     <div className={styles.content}>
-      <div className={styles.content__text}>implant</div>
+      <div className={styles.content__text}>
+        <div className={styles.content__title}>Implant</div>
+        <div className={styles.content__desc}>
+          Our brain-computer interface is fully implantable, cosmetically
+          invisible, and designed to let you control a computer or mobile device
+          anywhere you go.
+        </div>
+      </div>
       <div className={styles.content__img}>img</div>
     </div>
   );

dff7bb7ad19ad2c0dd43cfbffbbad8e6b457bb2d
Sumi <equepem@gmail.com>
2023-09-28 21:07:07 +0200

    docs/modify-data-json-file

--- app/components/PresentationLists.tsx
+++ app/components/PresentationLists.tsx
@@ -1,5 +1,5 @@
 async function getPresentations() {
-  const res = await fetch("http://localhost:4000/tickets", {
+  const res = await fetch("http://localhost:4000/projects", {
     next: {
       revalidate: 0,
     },
@@ -11,8 +11,8 @@ export default async function PresentationLists() {
   const presentations = await getPresentations();
   return (
     <>
-      {presentations.map((ticket: any) => (
-        <div key={ticket.id}>{ticket.title}</div>
+      {presentations.map((project: any) => (
+        <div key={project.id}>{project.title}</div>
       ))}
     </>
   );

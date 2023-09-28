635f309a1bc8e9c4e9a1aeae17deb9344e13564b
Sumi <equepem@gmail.com>
2023-09-28 21:20:22 +0200

    feature/create-presentation-bar-with-mock-json

--- app/components/PresentationBar.tsx
+++ app/components/PresentationBar.tsx
@@ -3,21 +3,27 @@ import styles from "../styles/components/PresentationBar.module.scss";
 //comp
 import PresentationLink from "./PresentationLink";
 
-export default function PresentationBar(): JSX.Element {
+async function getProjects() {
+  const response = await fetch("http://localhost:4000/projects", {
+    next: {
+      revalidate: 60,
+    },
+  });
+  return await response.json();
+}
+
+export default async function PresentationBar() {
+  const projects = await getProjects();
+
   return (
     <div className={styles.bar}>
-      <div className={styles.bar__link}>
-        <PresentationLink name="AI-Concierge" />
-      </div>
-      <div className={styles.bar__link}>
-        <PresentationLink name="Contacky" />
-      </div>
-      <div className={styles.bar__link}>
-        <PresentationLink name="My Code !" />
-      </div>
-      <div className={styles.bar__link}>
-        <PresentationLink name="Harry Potter" />
-      </div>
+      {projects.map((project: any) => {
+        return (
+          <div key={project.id} className={styles.bar__link}>
+            <PresentationLink name={project.title} />
+          </div>
+        );
+      })}
     </div>
   );
 }

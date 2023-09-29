6da69b70462e076495d9cca8437b3aaa23b5723e
Sumi <equepem@gmail.com>
2023-09-29 15:30:11 +0200

    feature/add-circle-on-main-phrase:

--- app/components/MainPhrase.tsx
+++ app/components/MainPhrase.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image";
-import NeonPurple from "../../public/purple-neon.gif";
 import styles from "../styles/components/MainPhrase.module.scss";
 
 export default function MainPhrase(): JSX.Element {
   return (
     <div className={styles.phrase}>
-      {/* <Image className={styles.img} src={NeonPurple} alt="neon=purple" fill /> */}
       <div className={styles.phrase__container}>
-        <div className={styles.title}>
+        {/* <div className={styles.title}>
           as a <span>Software Engineer</span>
-        </div>
+        </div> */}
       </div>
       <div className={styles.phrase__container}>
+        <div className={styles.circle}></div>
         <div className={styles.circle}></div>
       </div>
     </div>

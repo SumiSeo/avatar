61790014236b05e8b9f5028cecaac8f836bfdc04
Sumi <equepem@gmail.com>
2023-09-29 15:11:00 +0200

    feature/add-styling-to-main-phrase:

--- app/components/MainPhrase.tsx
+++ app/components/MainPhrase.tsx
@@ -4,9 +4,16 @@ import styles from "../styles/components/MainPhrase.module.scss";
 
 export default function MainPhrase(): JSX.Element {
   return (
-    <>
+    <div className={styles.phrase}>
       {/* <Image className={styles.img} src={NeonPurple} alt="neon=purple" fill /> */}
-      <div className={styles.title}>as a</div>
-    </>
+      <div className={styles.phrase__container}>
+        <div className={styles.title}>
+          as a <span>Software Engineer</span>
+        </div>
+      </div>
+      <div className={styles.phrase__container}>
+        <div className={styles.circle}></div>
+      </div>
+    </div>
   );
 }
--- app/components/SkipBar.tsx
+++ app/components/SkipBar.tsx
@@ -6,7 +6,7 @@ export default function Footer(): JSX.Element {
     <div className={styles.skipBar}>
       <div className={styles.skipBar__box}>
         <Link className={styles.skipBar__link} href="/test">
-          Skip intro
+          Skip Intro
         </Link>
       </div>
       <div className={styles.skipBar__box}></div>

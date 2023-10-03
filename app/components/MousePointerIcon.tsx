795fe8036f743cff5b3f671fbbdfce288e3c8b55
Sumi <equepem@gmail.com>
2023-10-03 22:17:12 +0200

    feature/appear-blue-circle-for-mouse-cursor

--- app/components/MousePointerIcon.tsx
+++ app/components/MousePointerIcon.tsx
@@ -2,20 +2,23 @@ import styles from "../styles/components/MousePointerIcon.module.scss";
 
 export default function MousePointerIcon(): JSX.Element {
   return (
-    <div className={styles.pointer}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="36"
-        height="36"
-        viewBox="0 0 24 24"
-        stroke="#ffffff"
-        fill="#000000"
-        strokeWidth="1.3"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M4 4l7.07 17 2.51-7.39L21 11.07z" />
-      </svg>
-    </div>
+    <>
+      <div className={styles.pointer}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+          stroke="#ffffff"
+          fill="#000000"
+          strokeWidth="1.3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M4 4l7.07 17 2.51-7.39L21 11.07z" />
+        </svg>
+      </div>
+      <div className={styles.pointer__circle}></div>
+    </>
   );
 }

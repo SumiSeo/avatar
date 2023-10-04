c58d6fc4650db4a77ed0ccbaf1214fc8c96b92e9
Sumi <equepem@gmail.com>
2023-10-04 16:36:19 +0200

    feature/create-mouse-cursor-start-circle

--- app/components/MousePointerIcon.tsx
+++ app/components/MousePointerIcon.tsx
@@ -19,6 +19,7 @@ export default function MousePointerIcon(): JSX.Element {
         </svg>
       </div>
       <div className={styles.pointer__circle}></div>
+      <div className={styles.pointer__cursor}></div>
     </>
   );
 }

2cf178b88b22ab68fa37bb4de386bd9a6182cc12
Sumi <equepem@gmail.com>
2023-10-04 15:59:44 +0200

    feature/create-form-container-add-scss-styling

--- app/contact/CreateForm.tsx
+++ app/contact/CreateForm.tsx
@@ -9,6 +9,9 @@ export default function CreateForm(): JSX.Element {
   const [name, setName] = useState<String>("");
   const [email, setEmail] = useState<String>("");
   const [message, setMessage] = useState<String>("");
+  const [company, setCompany] = useState<String>("");
+  const [jobTitle, setJobTitle] = useState<String>("");
+  const [phone, setPhone] = useState<String>("");
   const [region, setRegion] = useState<String>("Please select an option");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
@@ -16,18 +19,12 @@ export default function CreateForm(): JSX.Element {
     //post request
     e.preventDefault();
     setIsLoading(true);
-    const ticket = {
-      title,
-      body,
-      priority,
-      user_email: "mario@netnija.dev",
-    };
+
     const response = await fetch("http://localhost:4000/presentations", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(ticket),
     });
     if (response.status === 201) {
       router.refresh();
@@ -38,35 +35,60 @@ export default function CreateForm(): JSX.Element {
   return (
     <div>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <label>
-          <span>Name: </span>
-          <input
-            required
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>Email: </span>
-          <input
-            required
-            type="text"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>Body: </span>
-          <textarea required onChange={(e) => setMessage(e.target.value)} />
-        </label>
-        <label>
-          <span>Prioirty: </span>
-          <select onChange={(e) => setRegion(e.target.value)}>
-            <option value="APAC">APAC</option>
-            <option value="US">US</option>
-            <option value="Europe">Europe</option>
-            <option value="Other">Other</option>
-          </select>
-        </label>
+        <div className={styles.form__container}>
+          <div className={styles.form__box}>
+            <input
+              required
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className={styles.form__box}>
+            <select onChange={(e) => setRegion(e.target.value)}>
+              <option value="APAC">APAC</option>
+              <option value="US">US</option>
+              <option value="Europe">Europe</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+        </div>
+        <div className={styles.form__container}>
+          <div className={styles.form__box}>
+            <input
+              required
+              type="text"
+              onChange={(e) => setCompany(e.target.value)}
+            />
+          </div>
+          <div className={styles.form__box}>
+            <input
+              required
+              type="text"
+              onChange={(e) => setJobTitle(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className={styles.form__container}>
+          <div className={styles.form__box}>
+            <input
+              required
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={styles.form__box}>
+            <input
+              required
+              type="text"
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className={styles.form__container}>
+          <div className={styles.form__box}>
+            <textarea required onChange={(e) => setMessage(e.target.value)} />
+          </div>
+        </div>
         <button disabled={isLoading}>
           {isLoading && <span>Adding...</span>}
           {!isLoading && <span>Add Ticket</span>}

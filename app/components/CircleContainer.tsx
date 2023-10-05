ec8763f69acea774adefed4199ff09cf3c630ad6
Sumi <equepem@gmail.com>
2023-10-05 15:01:07 +0200

    feature/disappear-circles-after-6.2secs

--- app/components/CircleContainer.tsx
+++ app/components/CircleContainer.tsx
@@ -11,24 +11,34 @@ import TooltipBubble from "./TooltipBubble";
 export default function CircleContainer(): JSX.Element {
   const { disappearCircles, setDisappearCircles } = useGlobalContext();
   useEffect(() => {
-    console.log("checking context circle", disappearCircles);
-    setDisappearCircles(true);
+    const timer = setTimeout(() => {
+      setDisappearCircles(true), console.log("after five seconds called");
+    }, 6200);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [disappearCircles]);
 
   return (
-    <div className={styles.phrase}>
-      <div className={styles.phrase__container}>
-        <div className={styles.chat__conatainer}>
-          <MousePointerIcon />
-          <TooltipBubble />
+    <>
+      {disappearCircles ? (
+        <div></div>
+      ) : (
+        <div className={styles.phrase}>
+          <div className={styles.phrase__container}>
+            <div className={styles.chat__conatainer}>
+              <MousePointerIcon />
+              <TooltipBubble />
+            </div>
+          </div>
+          <div className={styles.phrase__container}>
+            <div className={styles.circle}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.circle}></div>
+          </div>
         </div>
-      </div>
-      <div className={styles.phrase__container}>
-        <div className={styles.circle}></div>
-        <div className={styles.circle}></div>
-        <div className={styles.circle}></div>
-        <div className={styles.circle}></div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }

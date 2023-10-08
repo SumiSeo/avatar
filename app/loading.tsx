a1cd8a1b78b09b420a5450402d9afa9a0bc0e44d
Sumi <equepem@gmail.com>
2023-10-08 15:36:53 +0200

    chore/change-loading-com

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
 import styles from "./styles/Loading.module.scss";
 
 export default function Loading(): JSX.Element {
-  return <div className={styles.loading}>Loading...</div>;
+  return <div className={styles.loading}>...</div>;
 }

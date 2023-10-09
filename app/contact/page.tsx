496a801dd0086f5e8fa45408204239477600c83d
Sumi <equepem@gmail.com>
2023-10-09 13:35:28 +0200

    feature/modify-contact-page-design

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,7 +13,9 @@ export default function CreateContact() {
   return (
     <main className={styles.contact}>
       <div className={styles.contact__container}>
-        <h1>Let's work together</h1>
+        <h1>
+          Do you want to discuss more about your company or personal project?
+        </h1>
       </div>
       <div className={styles.contact__container}>
         <CreateForm />

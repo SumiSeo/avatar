77367a88402a3177c94db7bd0b73904ba97397ed
Sumi <equepem@gmail.com>
2023-10-10 15:02:54 +0200

    feature/figure-out-apollo-client

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./styles/globals.scss";
 
-// import withApollo from "next-with-apollo";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-
 import { GlobalContextProvider } from "./context/store";
 
 // components
@@ -13,10 +10,6 @@ import NavBar from "./components/NavBar";
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 // creating the Apollo Client
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_HASURA_APP_URL, // <- Configure GraphQL Server URL (must be absolute)
-  cache: new InMemoryCache(),
-});
 export const metadata: Metadata = {
   title: "Sumi SEO",
   description:

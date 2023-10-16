47d026a0ea2dce8ae0d19a7dd9c7454914063ad9
Sumi <equepem@gmail.com>
2023-10-16 15:52:00 +0200

    fix/image-loading-error-with-next-js

--- app/components/ContentImg.tsx
+++ app/components/ContentImg.tsx
@@ -5,26 +5,11 @@ import src3 from "@/public/create.gif";
 import src4 from "@/public/believe.gif";
 import src5 from "@/public/self-study.gif";
 
-//comp
-import ContentDevelop from "./ContentDevelop";
-
 export default function ContentImg({ id }: { id: number }): JSX.Element {
-  let source;
-  if (id === 1) {
-    source = src1;
-  }
   return (
     <div>
-      {id === 2 ? (
-        <ContentDevelop />
-      ) : (
-        <Image
-          src={source ? source : src1}
-          alt="human"
-          width={400}
-          height={0}
-        />
-      )}
+      {id === 1 ? <Image src={src1} alt="human" width={400} height={0} /> : ""}
+      {id === 2 ? <Image src={src2} alt="human" width={400} height={0} /> : ""}
       {id === 3 ? <Image src={src3} alt="human" width={400} height={0} /> : ""}
       {id === 4 ? <Image src={src4} alt="human" width={400} height={0} /> : ""}
       {id === 5 ? <Image src={src5} alt="human" width={400} height={0} /> : ""}

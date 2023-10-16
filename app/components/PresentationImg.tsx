6d5d1bfc23eee08d317f319367680abfdf821c25
Sumi <equepem@gmail.com>
2023-10-16 16:50:12 +0200

    chore/add-fluuent-img

--- app/components/PresentationImg.tsx
+++ app/components/PresentationImg.tsx
@@ -2,17 +2,9 @@ import Image from "next/image";
 import styles from "../styles/components/PresentationImg.module.scss";
 import Contacky from "../../public/contact-app.jpg";
 import AiConcierge from "../../public/ai-video.gif";
+import Fluuent from "../../public/fluuent-2.jpg";
 
-//comp
-import PresentationFooter from "./PresentationFooter";
-
-export default function PresentationImg({
-  id,
-  desc,
-}: {
-  id: number;
-  desc: string[];
-}): JSX.Element {
+export default function PresentationImg({ id }: { id: number }): JSX.Element {
   return (
     <div>
       {id === 1 ? (
@@ -27,6 +19,11 @@ export default function PresentationImg({
       )}
       {id === 2 ? (
         <Image fill className={styles.img} src={Contacky} alt="descritioin" />
+      ) : (
+        ""
+      )}{" "}
+      {id === 6 ? (
+        <Image fill className={styles.img} src={Fluuent} alt="descritioin" />
       ) : (
         ""
       )}

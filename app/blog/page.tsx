b5c159be7a408d3911aed953ebcb98996500f62e
Sumi <equepem@gmail.com>
2023-10-16 13:32:11 +0200

    feat/add-blog-page-and-modify-height-for-pageNotFound

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Blog.module.scss";
+
+export default function Blog(): JSX.Element {
+  return (
+    <div className={styles.blog}>
+      <h1>Coming soon...</h1>
+      <h3>Blog is under construction</h3>
+    </div>
+  );
+}

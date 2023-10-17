bf727f42176c596492d9a4a0ca8f5bb2f129fd42
Sumi <equepem@gmail.com>
2023-10-17 13:36:34 +0200

    feat/add-project-gifs

--- app/components/PresentationImg.tsx
+++ app/components/PresentationImg.tsx
@@ -3,6 +3,9 @@ import styles from "../styles/components/PresentationImg.module.scss";
 import Contacky from "../../public/contact-app.jpg";
 import AiConcierge from "../../public/ai-video.gif";
 import Fluuent from "../../public/fluuent-2.jpg";
+import MyCode from "../../public/mycode.gif";
+import HarryPotter from "../../public/harrypotter.gif";
+import WeatherUp from "../../public/weatherup.gif";
 
 export default function PresentationImg({ id }: { id: number }): JSX.Element {
   return (
@@ -21,7 +24,27 @@ export default function PresentationImg({ id }: { id: number }): JSX.Element {
         <Image fill className={styles.img} src={Contacky} alt="descritioin" />
       ) : (
         ""
-      )}{" "}
+      )}
+      {id === 3 ? (
+        <Image fill className={styles.img} src={MyCode} alt="descritioin" />
+      ) : (
+        ""
+      )}
+      {id === 4 ? (
+        <Image fill className={styles.img} src={WeatherUp} alt="descritioin" />
+      ) : (
+        ""
+      )}
+      {id === 5 ? (
+        <Image
+          fill
+          className={styles.img}
+          src={HarryPotter}
+          alt="descritioin"
+        />
+      ) : (
+        ""
+      )}
       {id === 6 ? (
         <Image fill className={styles.img} src={Fluuent} alt="descritioin" />
       ) : (

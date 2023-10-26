1d25979ad505f0cef544563f1d0b27f122264a43
Sumi <equepem@gmail.com>
2023-10-26 22:33:53 +0200

    feat/finish-keep-reading-comp-styling

--- app/components/KeepReading.tsx
+++ app/components/KeepReading.tsx
@@ -1,3 +1,10 @@
+import style from "../styles/components/KeepReading.module.scss";
+
 export default function KeepReading(): JSX.Element {
-  return <div>Keep reading</div>;
+  return (
+    <div className={style.keepReading}>
+      <div className={style.keepReading__container}>Keep Reading </div>
+      <div className={style.keepReading__container}> &#8599;</div>
+    </div>
+  );
 }

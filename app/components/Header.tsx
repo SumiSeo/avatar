09324e685b2f79b7ff59dff60d7e0b6bae4b9286
Sumi <equepem@gmail.com>
2023-10-27 15:44:02 +0200

    feature/create-write-icon-com

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,14 @@
 import styles from "../styles/components/Header.module.scss";
+import WriteIcon from "./WriteIcon";
 
 export default function Header({ title }: { title: String }): JSX.Element {
   return (
     <>
       <div className={styles.header}>{title}</div>
       <div className={styles.write}>
-        <div className={styles.write__container}></div>
+        <div className={styles.write__container}>
+          <WriteIcon />
+        </div>
         <div className={styles.write__container}>Write</div>
       </div>
     </>

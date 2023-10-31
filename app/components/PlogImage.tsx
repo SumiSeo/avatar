a44c9bf798acb9765b31696206ef46394103f441
Sumi <equepem@gmail.com>
2023-10-31 14:36:52 +0100

    feat/create-heart-and-plus-icons

--- app/components/PlogImage.tsx
+++ app/components/PlogImage.tsx
@@ -2,20 +2,23 @@ import sample from "../../public/fluuent.jpg";
 import Image from "next/image";
 import styles from "../styles/components/PlogImage.module.scss";
 
-import KeepReading from "./KeepReading";
-
 export default function PlogImage(): JSX.Element {
   const id = 1;
   return (
     <div className={styles.plog}>
       <div className={styles.plog__container}>
-        <Image width={600} alt="sample" src={sample} />
+        <Image
+          objectFit="cover"
+          layout="responsive"
+          alt="sample"
+          src={sample}
+        />
       </div>
-      <div className={styles.plog__title}>NextJS(3)</div>
-      <div className={styles.plog__date}>3 September 2023</div>
-      <div className={styles.plog__more}>
-        <KeepReading id={id} />
+      <div className={styles.plog__icons}>
+        <div className={styles.plog__like}>&#x2764;</div>
+        <div className={styles.plog__more}>&#x2b;</div>
       </div>
+      <div className={styles.plog__title}>NextJS Crush 01</div>
     </div>
   );
 }

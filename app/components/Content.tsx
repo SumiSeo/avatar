a2de97f33bf4a4aeff77e8d7fa4dfd2cf91bdef4
Sumi <equepem@gmail.com>
2023-11-01 16:26:22 +0100

    feat/add-circle-to-content-bar"

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -12,8 +12,18 @@ export default function Content({
   return (
     <div className={styles.content}>
       <div className={styles.content__text}>
-        <div className={styles.content__title}>{mission_title}</div>
-        <div className={styles.content__desc}>{mission_description}</div>
+        <div className={styles.content__title}>
+          Frontend Developer, Fluuent; Portland, USA
+        </div>
+        <div className={styles.content__desc}>
+          • Developed medical referral application to improve the American
+          healthcare system <br />
+          <br />• R&D development with Microsoft PCF, C#, and Blazor Framework
+          to attempt for polling Back-End data consistently to the user's local
+          browser cache
+          <br />
+          <br />• Technologies : .NET, C#, Blazor, and JavaScript
+        </div>
       </div>
       <div className={styles.content__img}>
         <ContentImg id={mission_id} />
--- app/components/ContentBar.tsx
+++ app/components/ContentBar.tsx
@@ -13,16 +13,22 @@ export default function ContentBar({
 }): JSX.Element {
   const active = styles.contentBar__active;
   const nonActive = styles.contentBar__box;
+  const nonActiveCircle = styles.content__nocircle;
 
   return (
-    <div className={styles.contentBar}>
+    <div className={styles.content}>
+      <div className={styles.contentBar}>
+        <span
+          data-key={title}
+          key={id}
+          className={clicked === id - 1 ? active : nonActive}
+        >
+          <div className={styles.about__box}></div>
+        </span>
+      </div>
       <span
-        data-key={title}
-        key={id}
-        className={clicked === id - 1 ? active : nonActive}
-      >
-        <div className={styles.about__box}></div>
-      </span>
+        className={clicked == id - 1 ? styles.content__circle : nonActiveCircle}
+      ></span>
     </div>
   );
 }

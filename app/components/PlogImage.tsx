4b82dac51a9ea00a3f04b07507c758399ceec83c
Sumi <equepem@gmail.com>
2023-11-03 11:20:50 +0100

    test

--- app/components/PlogImage.tsx
+++ app/components/PlogImage.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useState } from "react";
 import sample from "../../public/fluuent.jpg";
 import Image from "next/image";
 import styles from "../styles/components/PlogImage.module.scss";
+import Link from "next/link";
 
-export default function PlogImage({ title }: { title: string }): JSX.Element {
-  const id = 1;
+export default function PlogImage({
+  title,
+  id,
+}: {
+  title: string;
+  id: number;
+}): JSX.Element {
+  const [heart, setHeart] = useState<boolean>(false);
   return (
     <div className={styles.plog}>
       <div className={styles.plog__container}>
@@ -15,8 +25,15 @@ export default function PlogImage({ title }: { title: string }): JSX.Element {
         />
       </div>
       <div className={styles.plog__icons}>
-        <div className={styles.plog__like}>&#x2764;</div>
-        <div className={styles.plog__more}>&#x2b;</div>
+        <div
+          onClick={() => setHeart(!heart)}
+          className={!heart ? styles.plog__like : styles.plog__dislike}
+        >
+          &#x2764;
+        </div>
+        <Link href={`/plog/${id}`}>
+          <div className={styles.plog__more}>&#x2b;</div>
+        </Link>
       </div>
       <div className={styles.plog__title}>{title}</div>
     </div>
